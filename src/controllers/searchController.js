86721ddbe9aa3cf9fcc04e0a12061a4079bd8251
thiagocifani <cifani.thiago@gmail.com>
2020-01-18 04:59:06 +0100

    Adds search controller

---
+++ src/controllers/searchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/dev');
+const parseStringToArray = require('../utils/parseStringToArray');
+
+class SearchController {
+  async index(req, res) {
+    const { latitude, longitude, techs } = req.query
+
+    const arrayTechs = parseStringToArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: arrayTechs
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        }
+      }
+    })
+
+    return res.json(devs);
+  }
+}
+
+module.exports = new SearchController();
