1dfb5d92ee064625d1c09b2f5d66fca20f8d1e2e
thiagocifani <cifani.thiago@gmail.com>
2020-01-18 04:58:48 +0100

    Adds dev controller.

---
+++ src/controllers/devController.js
@@ -0,0 +1,43 @@
+const Dev = require('../models/dev');
+const axios = require('axios');
+const parseStringToArray = require('../utils/parseStringToArray');
+
+class DevController {
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    return res.json(devs);
+  }
+
+  async create(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const response = await axios.get(`http://api.github.com/users/${github_username}`);
+
+      const { name = login, avatar_url, bio} = response.data;
+      
+      const arrayTechs = parseStringToArray(techs);
+
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      }
+      
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: arrayTechs,
+        location
+      });
+    }
+
+    return res.json(dev);
+  }
+}
+
+module.exports = new DevController();
